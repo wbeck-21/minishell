32c68724af1ed5198520508ed2460fd4a613e74c
Anastasia <goodmorning.25@mail.ru>
2022-09-20 22:38:01 +0300

    full main()

--- minishell.c
+++ minishell.c
@@ -15,16 +15,33 @@ t_lst	*envp_copy(char *envp[])
 	return (list);
 }
 
-void minishell(t_mini *mini, char **env, char **str_s)
+void minishell(t_mini *mini, char **envp, char **strs)
 {
 	int	i;
-	t_list *tmp;
+	t_list *temp;
 
 	i = -1;
 	while (1)
 	{
 		printf("OK");
 		sleep(1);
-		exec(mini, env);
+		dup2(mini->cmds.in, STDIN_FILENO);
+		dup2(mini->cmds.out, STDOUT_FILENO);
+		sigaction(SIGQUIT, &mini->sig, NULL);
+		sigaction(SIGINT, &mini->sig, NULL);
+		// strs[++i] = readline("minishell $> ");
+		// if (!result_line(&strs[i], &mini->history, mini))
+		// 	continue ;
+		// mini->list = do_split(strs[i]); //  нужнен массив строк
+		// free(strs[i]);
+		temp = mini->list;
+		while (temp)
+		{
+			temp->content = insert_inside_gap2(mini->change, temp->content);
+			temp = temp->next;
+		}
+		make_split(&mini->list);
+		exec(mini, envp);
+		// special_free(mini);
 	}
 }
